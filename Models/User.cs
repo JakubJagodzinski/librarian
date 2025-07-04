a3c6c1dcfa9a6937a31158eb86aca0db09b4477f
JagodaJakub <kubajagjag@gmail.com>
2025-07-04 03:57:11 +0200

    Add User table to introduce authorization

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿public class User
+{
+    public int AppUserId { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string HashedPassword { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+    public int ForeignKeyId { get; set; }
+    public string UserType { get; set; } = string.Empty;
+}
---
+++ Utils/PasswordHasher.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+public static class PasswordHasher
+{
+    public static string Hash(string password)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 10000,
+            numBytesRequested: 256 / 8));
+
+        return $"{Convert.ToBase64String(salt)}.{hashed}";
+    }
+
+    public static bool Verify(string password, string hashedWithSalt)
+    {
+        var parts = hashedWithSalt.Split('.');
+        if (parts.Length != 2) return false;
+
+        byte[] salt = Convert.FromBase64String(parts[0]);
+
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 10000,
+            numBytesRequested: 256 / 8));
+
+        return hashed == parts[1];
+    }
+}
