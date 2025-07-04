ef9b68e160888bba32896d38a622dc7b1ae4ae6c
JagodaJakub <kubajagjag@gmail.com>
2025-07-04 21:26:25 +0200

    Fix navigation

--- Models/BlacklistedReader.cs
+++ Models/BlacklistedReader.cs
@@ -4,7 +4,7 @@
     {
         public int BlacklistedReaderId { get; set; }
         public int ReaderId { get; set; }
-        public Reader Reader { get; set; } = new Reader();
+        public Reader Reader { get; set; }
 
         public string Reason { get; set; } = string.Empty;
         public DateTime BlacklistedDate { get; set; }
--- Models/Book.cs
+++ Models/Book.cs
@@ -5,9 +5,10 @@
         public int BookId { get; set; }
         public string Title { get; set; } = string.Empty;
         public int AuthorId { get; set; }
-        public Author Author { get; set; } = new Author();
+        public Author Author { get; set; }
         public int PublishedYear { get; set; }
         public int Pages { get; set; }
+        public int InStock { get; set; }
         public ICollection<BookGenre> BookGenres { get; set; } = new List<BookGenre>();
     }
 }
